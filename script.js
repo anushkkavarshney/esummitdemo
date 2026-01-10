af66e8b2ee55ae972e2e2e82ff4be09f0fcebfc6
anushkkavarshney <vikashvarshney33@gmail.com>
2026-01-10 20:29:36 +0530

    footer and back to top button added

--- script.js
+++ script.js
@@ -208,6 +208,26 @@ const initSquares = () => {
 // Start Systems
 initSquares();
 // initChrome();
+/* =========================
+   Scroll To Top Button
+========================= */
+
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add('show');
+  } else {
+    scrollTopBtn.classList.remove('show');
+  }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
 
 /**
  * ===============================================
